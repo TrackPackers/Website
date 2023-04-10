a8a2401e9ba7e72083b509393c88d2011fa69412
BartVnim <65620353+BartvNim@users.noreply.github.com>
2023-04-10 21:10:16 +0200

    update way of exporting

--- src/utils/API/createNewPost.tsx
+++ src/utils/API/createNewPost.tsx
@@ -8,8 +8,10 @@ interface Response{
     message: string;
 }
 
-export default function createNewPost(data: Request) {
+function createNewPost(data: Request) {
     return axios.post<Response>(
         `${process.env.REACT_APP_GATEWAY_URL}/contentwriter/new`, data,
     );
 }
+
+export default createNewPost;
