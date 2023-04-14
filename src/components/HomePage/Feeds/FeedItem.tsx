592f111f0d9c297cfc225994e4c90899d38e3d5e
BartvNim <65620353+BartvNim@users.noreply.github.com>
2023-04-14 13:15:29 +0200

    Newest post feed (#11)
    Close: #6

---
+++ src/components/HomePage/Feeds/FeedItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Post } from "../../../utils/Interfaces/Post";
+interface Props {
+    post: Post;
+}
+
+function FeedItem(props: Props) {
+    return (
+      <div className="container">
+        <h2>{props.post.message} - {props.post.name}</h2>
+      </div>
+    );
+  }
+  
+  export default FeedItem;
