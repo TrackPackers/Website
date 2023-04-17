222e2771902ea9a88210aabfb3c13884202660cd
BartvNim <65620353+BartvNim@users.noreply.github.com>
2023-04-17 12:43:21 +0200

    Update .lighthouserc.cjs

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -4,8 +4,8 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     collect: {
-      startserverCommand: 'npm run dev',
-      url: ['http://localhost:5173'],
+      startserverCommand: 'npm run preview',
+      url: ['http://localhost:4173'],
       numberofRuns: 3,
     },
   },
