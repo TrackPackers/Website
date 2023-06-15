8cd4914447b727c0bf3998df2f558125e575fdc4
BartVnim <65620353+BartvNim@users.noreply.github.com>
2023-06-15 10:10:01 +0200

    add testcase to name

--- cypress/e2e/testFeedViews.cy.js
+++ cypress/e2e/testFeedViews.cy.js
@@ -1,7 +1,7 @@
 // Because top posts are currently not implemented, it should display "No top posts available"
 
 describe('Clicking the top button should show correct Text"', () => {
-  it('should change the text of another element', () => {
+  it('UC5:TC2: should change the text of another element', () => {
     cy.visit('http://localhost:4173');
 
     cy.contains('a', 'Top').click();
